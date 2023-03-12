e069383254c4a2af401145afb1c7c98b8e99c5b9
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 06:22:45 +0300

    Add ChatCompletionRepository implementation with network and remote data sources to complete chat requests

---
+++ lib/features/chat_completion/data/repository/chat_completion_repository_implementation.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/core.dart';
+import '../../domain/domain.dart';
+import '../data.dart';
+
+class ChatCompletionRepository implements IChatCompletionRepository {
+  final IChatCompletionRemoteDataSource _remoteDataSource;
+  final INetworkInfo _networkInfo;
+
+  const ChatCompletionRepository({
+    required IChatCompletionRemoteDataSource remoteDataSource,
+    required INetworkInfo networkInfo,
+  })  : _remoteDataSource = remoteDataSource,
+        _networkInfo = networkInfo;
+
+  @override
+  Future<Either<BaseFailure, ChatCompletionMessageEntity>> completeChat({required ChatCompletionRequest request}) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final response = await _remoteDataSource.completeChat(request: request);
+        return right(response.choices.first.message);
+      } on BaseException catch (exception) {
+        return left(BaseFailure(message: exception.message));
+      } catch (exception) {
+        return left(BaseFailure(message: exception.toString()));
+      }
+    } else {
+      return left(const BaseFailure.noInternetConnection());
+    }
+  }
+}
