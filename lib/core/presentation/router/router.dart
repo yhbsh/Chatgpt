3f4d27764fdfd8b0f48e1a0584a004a5991b386b
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 05:57:02 +0300

    Refactor import statement for chat feature to chat_completion in router.dart

--- lib/core/presentation/router/router.dart
+++ lib/core/presentation/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../features/chat/chat.dart';
+import '../../../features/chat_completion/chat_completion.dart';
 
 final routerProvider = Provider<GoRouter>(
   (ref) {
