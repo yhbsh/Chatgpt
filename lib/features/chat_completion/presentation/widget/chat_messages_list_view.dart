6ebdb091dd969f4c7d8b9095654562f4433e85af
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 09:39:46 +0300

    Add ChatMessagesListView widget for displaying chat messages in the chat completion feature

---
+++ lib/features/chat_completion/presentation/widget/chat_messages_list_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../presentation.dart';
+
+class ChatMessagesListView extends ConsumerWidget {
+  const ChatMessagesListView({super.key, required this.messages});
+
+  final List<String> messages;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // get last message from chat history
+    final chatHistory = ref.watch(chatHistoryNotifierProvider);
+
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+          child: Column(
+            children: List.generate(
+              messages.length,
+              (index) {
+                // either requested(Loading) state or initial state
+                if (chatHistory.length <= index) {
+                  return Column(
+                    children: [
+                      SentMessageCard(message: messages.elementAt(index)),
+                      const SizedBox(height: 16),
+                      const ShimmerMessagePlaceHolder(),
+                    ],
+                  );
+                }
+
+                // either success or failure state
+                return SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      SentMessageCard(message: messages.elementAt(index)),
+                      const SizedBox(height: 16),
+                      RecievedMessageCard(
+                        message: index == 0 //
+                            ? chatHistory.elementAt(index).content.split('\n').sublist(1).join()
+                            : chatHistory.elementAt(index).content,
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
