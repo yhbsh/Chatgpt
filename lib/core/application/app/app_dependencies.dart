02a4049c8faad4472b641a3a4ab1a818fb062b24
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 06:20:50 +0300

    Add Dio provider to app dependencies in order to handle network requests

--- lib/core/application/app/app_dependencies.dart
+++ lib/core/application/app/app_dependencies.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../core.dart';
 
 final networkInfoProvider = Provider<INetworkInfo>((ref) => NetworkInfo());
+final dioProvider = Provider<Dio>((ref) => Dio());
