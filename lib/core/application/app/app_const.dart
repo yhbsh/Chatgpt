13aa887a04537d4d0a3f800fcc7e321e7a880d6a
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 08:13:02 +0300

    Refactor AppConst to use an environment variable for the OpenAI API key

--- lib/core/application/app/app_const.dart
+++ lib/core/application/app/app_const.dart
@@ -1,6 +1,6 @@
 class AppConst {
   const AppConst._();
 
-  static const String openAIApiKey = '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';
+  static const String openAIApiKey = String.fromEnvironment('OPENAI_API_KEY');
   static const String openAIApiBaseUrl = 'https://api.openai.com/v1/chat';
 }
