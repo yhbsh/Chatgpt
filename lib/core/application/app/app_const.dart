e2945c5a00828b12bb0a5e2dddb9266d438ce202
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 07:53:33 +0300

    Update openAI API key in AppConst class to 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

--- lib/core/application/app/app_const.dart
+++ lib/core/application/app/app_const.dart
@@ -1,6 +1,6 @@
 class AppConst {
   const AppConst._();
 
-  static const String openAIApiKey = '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';
+  static const String openAIApiKey = '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';
   static const String openAIApiBaseUrl = 'https://api.openai.com/v1/chat';
 }
