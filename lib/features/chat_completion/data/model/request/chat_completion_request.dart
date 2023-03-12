dd7efa8baaf14ec2b4a38b1e17e6c5455d77137d
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 12:33:06 +0300

    Add new required fields for ChatCompletionRequest with temperature, top_p, n, presence_penalty, frequency_penalty, and logit_bias in data model

--- lib/features/chat_completion/data/model/request/chat_completion_request.dart
+++ lib/features/chat_completion/data/model/request/chat_completion_request.dart
@@ -12,6 +12,12 @@ class ChatCompletionRequest with _$ChatCompletionRequest {
   const factory ChatCompletionRequest({
     @JsonKey(name: 'model') required String model,
     @JsonKey(name: 'messages') required List<ChatCompletionMessageEntity> messages,
+    @JsonKey(name: 'temperature') required double temperature,
+    @JsonKey(name: 'top_p') required double topP,
+    @JsonKey(name: 'n') required int n,
+    @JsonKey(name: 'presence_penalty') required double presencePenalty,
+    @JsonKey(name: 'frequency_penalty') required double frequencyPenalty,
+    @JsonKey(name: 'logit_bias') required Map<String, double> logitBias,
   }) = _ChatCompletionRequest;
 
   factory ChatCompletionRequest.fromJson(Map<String, dynamic> json) => _$ChatCompletionRequestFromJson(json);
