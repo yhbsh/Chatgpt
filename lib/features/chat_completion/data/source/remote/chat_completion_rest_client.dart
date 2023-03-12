6d8d8d5b55b22b0d4fb488e39569e4b7495777e1
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 07:12:20 +0300

    Refactor ChatCompletionRestClient to implement IChatCompletionRestClient interface and handle DioError

--- lib/features/chat_completion/data/source/remote/chat_completion_rest_client.dart
+++ lib/features/chat_completion/data/source/remote/chat_completion_rest_client.dart
@@ -1,16 +1,31 @@
-import 'package:dio/dio.dart' hide Headers;
-import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
 
 import '../../../../../core/core.dart';
 import '../../data.dart';
 
-part 'chat_completion_rest_client.g.dart';
-
-@RestApi(baseUrl: AppConst.openAIApiChatCompletionBaseUrl)
 abstract class IChatCompletionRestClient {
-  factory IChatCompletionRestClient(Dio dio, {String baseUrl}) = _IChatCompletionRestClient;
-
-  @POST('/completions')
-  @Headers({'Authorization': AppConst.openAIApiKey})
   Future<ChatCompletionResponse> completeChat({required ChatCompletionRequest request});
+}
+
+class ChatCompletionRestClient implements IChatCompletionRestClient {
+  final Dio _dio;
+
+  const ChatCompletionRestClient({required Dio dio}) : _dio = dio;
+
+  @override
+  Future<ChatCompletionResponse> completeChat({required ChatCompletionRequest request}) async {
+    try {
+      final response = await _dio.post<Map<String, dynamic>>(
+        '${AppConst.openAIApiBaseUrl}/completions',
+        data: request.toJson(),
+        options: Options(headers: {'Authorization': 'Bearer ${AppConst.openAIApiKey}'}),
+      );
+
+      return ChatCompletionResponse.fromJson(response.data!);
+    } on DioError {
+      throw const BaseException.serverError();
+    } catch (exception) {
+      rethrow;
+    }
+  }
 }
