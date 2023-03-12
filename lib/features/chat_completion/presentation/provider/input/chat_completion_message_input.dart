9b3ef96749076893d7775ff418696fb58488631f
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 07:50:40 +0300

    Refactor ChatCompletionMessageInput class to add factory method and toString() method

--- lib/features/chat_completion/presentation/provider/input/chat_completion_message_input.dart
+++ lib/features/chat_completion/presentation/provider/input/chat_completion_message_input.dart
@@ -8,4 +8,9 @@ class ChatCompletionMessageInput {
   const ChatCompletionMessageInput({required this.role, required this.content});
 
   ChatCompletionMessageEntity toEntity() => ChatCompletionMessageEntity(role: role.name, content: content);
+
+  factory ChatCompletionMessageInput.fromEntity(ChatCompletionMessageEntity entity) => ChatCompletionMessageInput(role: ChatCompletionMessageRole.values.firstWhere((e) => e.name == entity.role), content: entity.content);
+
+  @override
+  String toString() => 'ChatCompletionMessageInput(role: $role, content: $content)';
 }
