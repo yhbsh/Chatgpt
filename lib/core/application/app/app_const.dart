ea54b2fafe033d17f091e2fbfff2ac02fcbf07aa
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-03-12 06:31:32 +0300

    Add the OpenAI API chat completion base URL to the AppConst class in app_const.dart

--- lib/core/application/app/app_const.dart
+++ lib/core/application/app/app_const.dart
@@ -2,4 +2,5 @@ class AppConst {
   const AppConst._();
 
   static const String openAIApiKey = 'Bearer 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';
+  static const String openAIApiChatCompletionBaseUrl = 'https://api.openai.com/v1/chat/completions';
 }
